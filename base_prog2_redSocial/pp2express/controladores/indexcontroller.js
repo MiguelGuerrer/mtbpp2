fb805e263232d708f70f30c794ffaedfa67a0aed
Brucebe <bebert@udesa.edu.ar>
2022-09-16 15:52:53 -0300

    perfil, registracion y busqueda modificados

--- base_prog2_redSocial/pp2express/controladores/indexcontroller.js
+++ base_prog2_redSocial/pp2express/controladores/indexcontroller.js
@@ -1,10 +1,12 @@
 //requires
 
+const index = require('../db/index')
 
 //metodos
+
 const indexController = {
   home: function(req, res) {
-      res.render('index');
+      res.render('index', {posteos: index.posteos});
     },
     busqueda: function(req, res) {
       res.render('resultadoBusqueda');
